705794525a3226075d02a178de2d637a7144481b
frankpagan <frank@cocreate.app>
2023-10-24 02:12:04 -0600

    formating

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
-        define(["./client"], function(CoCreateUsers) {
-        	return factory(CoCreateUsers)
+        define(["./client"], function (CoCreateUsers) {
+            return factory(CoCreateUsers)
         });
     } else if (typeof module === 'object' && module.exports) {
-      const CoCreateUsers = require("./server.js")
-      module.exports = factory(CoCreateUsers);
+        const CoCreateUsers = require("./server.js")
+        module.exports = factory(CoCreateUsers);
     } else {
         root.returnExports = factory(root["./client.js"]);
-  }
+    }
 }(typeof self !== 'undefined' ? self : this, function (CoCreateUsers) {
-  return CoCreateUsers;
+    return CoCreateUsers;
 }));
